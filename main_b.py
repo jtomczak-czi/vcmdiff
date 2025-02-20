a72c05f0e72b0978f1b22f265067fc8bb69f277e
jtomczak-czi <jtomczak@chanzuckerberg.com>
2025-02-19 17:00:59 -0800

    First commit

---
+++ main_b.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+import torch
+from torch.utils.data import DataLoader
+
+import lightning.pytorch as pl
+from lightning.pytorch.loggers import CSVLogger
+from lightning.pytorch.callbacks import LearningRateMonitor, TQDMProgressBar, ModelCheckpoint, EarlyStopping
+
+from models.diffusion import DiffusionModel, ConditionalDiffusionModel, BinaryDiffusion
+from modules.nnets import DiT, UNet1D, ResNetTime, MLPTime, UNetMLP, UNetMLPbn, UNetMLP_new, UNetMLPb
+from utils.datasets import LoadNumpyData, DCTDataset, FourierDataset, NormalizedDataset, CountDataset, CountOccurenceDataset
+
+def run():
+    # Training setup
+    MINI_BATCH_train = 128
+    MINI_BATCH_val = 256
+    NUM_STEPS = 100
+
+    EPOCHS = 500
+    PATIENCE = 10
+
+    # Dataset
+    data_np = LoadNumpyData(file_name='data/pancreas.npy', ratios=(0.7, 0.15, 0.15))
+    train_dataset_np = data_np.get_data(data_type='train')
+    val_dataset_np = data_np.get_data(data_type='val')
+    test_dataset_np = data_np.get_data(data_type='test')
+
+    train_dataset = CountOccurenceDataset(train_dataset_np)
+    val_dataset = CountOccurenceDataset(val_dataset_np)
+    test_dataset = CountOccurenceDataset(test_dataset_np)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=MINI_BATCH_train, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=MINI_BATCH_val, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=MINI_BATCH_val, shuffle=False)
+
+    # Initialize model and trainer
+    # LOGGER
+    # csv_logger = CSVLogger("/mnt/czi-sci-ai/intrinsic-variation-gene-ex/project_gene_regulation/scDiff_data", name="diffusion.py")
+    csv_logger = CSVLogger("logging_space/scDiff_data", name="diffusion")
+
+    # MODEL
+    # -- Parameterization --
+    nnet = UNetMLPb(in_dim=1000, mid_dim=512, bottleneck_dim=512, num_steps=NUM_STEPS)
+
+    # -- Probabilistic Model --
+    model = BinaryDiffusion(nnet=nnet, timesteps=NUM_STEPS, beta_min=1e-4, beta_max=0.02, lr=1e-4, sequence_length=2000, T_max=100)
+
+    # TRAINER
+    # -checkpoints
+    progress_bar_callback = TQDMProgressBar(refresh_rate=50, leave=True)
+    early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=0.00, patience=PATIENCE, verbose=False, mode="min")
+    lr_callback = LearningRateMonitor(logging_interval="step")
+    model_checkpoint_callback = ModelCheckpoint(monitor='val_loss', mode='min', save_top_k=5)
+    # -trainer
+    trainer = pl.Trainer(max_epochs=EPOCHS, accelerator="cpu", devices=1, logger=csv_logger, callbacks=[lr_callback, early_stop_callback, progress_bar_callback, model_checkpoint_callback], log_every_n_steps=40)
+
+    # Train model
+    trainer.fit(model, train_dataloader, val_dataloader)
+
+    # Test model
+    trainer.test(model, dataloaders=test_dataloader)
+
+
+if __name__ == '__main__':
+    run()
+
+    print("-----===== Training is done =====-----")
---
+++ testing_model_b.py
@@ -0,0 +1,44 @@
+from collections import OrderedDict
+
+import torch
+
+from utils.evaluations import assess_gene_model1d, assess_gene_model1d_b
+from utils.datasets import LoadNumpyData, DCTDataset, NormalizedDataset, CountOccurenceDataset
+
+from models.diffusion import DiffusionModel, ConditionalDiffusionModel, BinaryDiffusion
+from modules.nnets import UNetMLP, UNetMLPbn, UNetMLPx, UNetMLPb
+
+if __name__ == "__main__":
+    with torch.no_grad():
+        # PATHS
+        result_dir = 'logging_space/scDiff_data/diffusion/version_0/checkpoints/'
+        name = 'epoch=101-step=8772.ckpt'
+
+
+        # MODEL
+        checkpoint_state_dict = torch.load(result_dir + name)
+        nnet_dict = checkpoint_state_dict['state_dict']
+
+        nnet_new_dict = OrderedDict()
+        for k in nnet_dict.keys():
+            nnet_new_dict[k[5:]] = nnet_dict[k]
+        del nnet_dict
+        NUM_STEPS = 50
+        # -nnet
+        nnet_best = UNetMLPb(in_dim=1000, mid_dim=512, bottleneck_dim=512, num_steps=NUM_STEPS)
+        nnet_best.load_state_dict(nnet_new_dict)
+        # -diffusion
+        model_best = BinaryDiffusion(nnet=nnet_best, timesteps=NUM_STEPS, beta_min=1e-4, beta_max=0.02, lr=1e-4, sequence_length=2000, T_max=100)
+
+        # DATA
+        data_np = LoadNumpyData(file_name='data/pancreas.npy', ratios=(0.7, 0.15, 0.15))
+        # train_dataset_np = data_np.get_data(data_type='train')
+        test_dataset_np = data_np.get_data(data_type='test')
+
+        # train_dataset = NormalizedDataset(train_dataset_np, normalize_counts=True)
+        # test_dataset = NormalizedDataset(test_dataset_np, normalize_counts=True, scale=train_dataset.scale)
+        test_dataset = CountOccurenceDataset(test_dataset_np)
+
+        # RUN ASSESSMENT
+        N = 1000
+        assess_gene_model1d_b(model_best, result_dir, name, test_dataset, N=N, C=1, D=1000, prior=False)
